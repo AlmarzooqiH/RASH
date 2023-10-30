9568b7063d969987a340bb0ba1df0b9e633b6595
AlmarzooqiH <AlmarzooqiH23@gmail.com>
2023-10-30 20:25:23 +0400

    Signup feature has been added

--- RASH/src/java/loginOrSignupServlet.java
+++ RASH/src/java/loginOrSignupServlet.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,22 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 
-/**
- *
- * @author truedeveloper
- */
 @WebServlet(urlPatterns = {"/loginOrSignupServlet"})
 public class loginOrSignupServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String db_URL = "jdbc:mysql://localhost:8889/RASH_HOTEL?autoReconnect=true&useSSL=false";
@@ -64,21 +47,30 @@ public class loginOrSignupServlet extends HttpServlet {
             }
         } else if (signup != null) {
             String user = request.getParameter("username");
+            if (checkUsername(user, statement)) {
+                response.getWriter().println("<center><h1>Username taken!</h1></center>");
+                response.getWriter().close();
+                return;
+            }
             String pass = request.getParameter("password");
             String fname = request.getParameter("firstname");
             String lname = request.getParameter("lastname");
             String email = request.getParameter("email");
             String pnumber = request.getParameter("phone-number");
             try {
-                int insertAccount = statement.executeUpdate("INSERT INTO `Account`(`Username`, `Password`) VALUES ('" + user + "','" + pass + "')");
-                ResultSet insertCustomer = statement.executeQuery("INSERT INTO `Customer`(`Fname`, `Lname`, `Phone#`, `AID`, `Email`) VALUES ("
+                statement.executeUpdate("INSERT INTO `Account`(`Username`, `Password`) VALUES ('" + user + "','" + pass + "')");
+                ResultSet insertAccount = statement.executeQuery("SELECT ID FROM Account WHERE Username='" + user + "'");
+                insertAccount.next();
+                int aid = insertAccount.getInt("ID");
+                statement.executeUpdate("INSERT INTO `Customer`(`Fname`, `Lname`, `Phone#`, `AID`, `Email`) VALUES ("
                         + "'" + fname + "',"
                         + "'" + lname + "',"
                         + "'" + pnumber + "',"
-                        + "'" + Integer.parseInt(statement.executeQuery("SELECT ID FROM Account WHERE Username='" + user +"'").getNString("ID")) + "',"
+                        + "'" + aid + "',"
                         + "'" + email + "')"
                 );
-
+                response.getWriter().println("<center><h1>Customer has been added successfully</h1></center>");
+                response.getWriter().close();
             } catch (Exception err) {
                 response.getWriter().println(err);
                 response.getWriter().close();
@@ -87,44 +79,22 @@ public class loginOrSignupServlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
     private boolean checkUsername(String username, Statement statement) {
         try {

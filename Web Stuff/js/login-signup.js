73db7795d716ef00675d73a389cd0239ded0fbae
AlmarzooqiH <AlmarzooqiH23@gmail.com>
2023-10-31 18:35:51 +0400

    Finished fixing the signup UI.

--- Web Stuff/js/login-signup.js
+++ Web Stuff/js/login-signup.js
@@ -5,7 +5,8 @@ function login() {
     const loginBtn = document.getElementById("login-btn");
     const signupBtn = document.getElementById("signup-btn");
     formBorder.style.height = "520px";
-    formBorder.style.transition = "height 0.6s";
+    formBorder.style.width = "600px";
+    formBorder.style.transition = "height 0.6s, width 0.6s";
     loginForm.style.visibility = "visible";
     signupForm.style.visibility = "hidden";
     loginBtn.style.backgroundColor = "#546ad5";
@@ -21,8 +22,8 @@ function signup() {
     const loginBtn = document.getElementById("login-btn");
     const signupBtn = document.getElementById("signup-btn");
     formBorder.style.height = "570px";
-    formBorder.style.width = "800px";
-    formBorder.style.transition = "height 0.6s";
+    formBorder.style.width = "1000px";
+    formBorder.style.transition = "height 0.6s, width 0.6s";
     loginForm.style.visibility = "hidden";
     signupForm.style.visibility = "visible";
     signupForm.style.marginLeft = "center";

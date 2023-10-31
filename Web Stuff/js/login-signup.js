803a0f1cb8596ab8b638448eba983dccc2a45459
AlmarzooqiH <AlmarzooqiH23@gmail.com>
2023-10-31 17:35:52 +0400

    Updated the login/signup form.

--- Web Stuff/js/login-signup.js
+++ Web Stuff/js/login-signup.js
@@ -4,29 +4,34 @@ function login() {
     const signupForm = document.getElementById("signup");
     const loginBtn = document.getElementById("login-btn");
     const signupBtn = document.getElementById("signup-btn");
-    formBorder.style.height = "400px";
+    formBorder.style.height = "520px";
     formBorder.style.transition = "height 0.6s";
     loginForm.style.visibility = "visible";
     signupForm.style.visibility = "hidden";
-    loginBtn.style.backgroundColor = "#d098fb";
+    loginBtn.style.backgroundColor = "#546ad5";
     loginBtn.style.color = "white";
     signupBtn.style.background = "transparent";
     signupBtn.style.cursor = "pointer";
 }
+
 function signup() {
     const formBorder = document.getElementById("login-signup-form");
     const loginForm = document.getElementById("login");
     const signupForm = document.getElementById("signup");
     const loginBtn = document.getElementById("login-btn");
     const signupBtn = document.getElementById("signup-btn");
-    formBorder.style.height = "600px";
+    formBorder.style.height = "570px";
+    formBorder.style.width = "800px";
     formBorder.style.transition = "height 0.6s";
     loginForm.style.visibility = "hidden";
     signupForm.style.visibility = "visible";
-    signupForm.style.marginLeft = "50px";
-    signupForm.style.marginTop = "-350px";
-    signupBtn.style.backgroundColor = "#d098fb";
+    signupForm.style.marginLeft = "center";
+    signupForm.style.marginTop = "center";
+    signupBtn.style.backgroundColor = "#546ad5";
     signupBtn.style.color = "white";
     loginBtn.style.cursor = "pointer";
     loginBtn.style.background = "transparent";
 }
+
+document.getElementById("login-btn").addEventListener("click", login);
+document.getElementById("signup-btn").addEventListener("click", signup);
